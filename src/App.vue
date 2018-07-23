47aedbffeb4224dce0ad74b6d8dda8f081fb63a7
Erick Patrick <contato@erickpatrick.net>
2018-07-24 00:07:55 +0200

    updates `App` frame layout by adding `Header` component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "RPGdM",
+  components: {
+    Header
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
