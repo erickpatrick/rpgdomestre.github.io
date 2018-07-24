822ddf66c40a8e6e45298a26dfa625b9b47def60
erick patrick <github@erickpatrick.net>
2018-07-24 14:52:15 +0200

    adds 'Navigation' and 'Footer' components and makes (sub)title dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :title="title" :subtitle="subtitle" />
+    <Navigation />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-
 export default {
   name: "RPGdM",
   components: {
-    Header
+    Header: () => import("@/components/Header.vue"),
+    Footer: () => import("@/components/Footer.vue"),
+    Navigation: () => import("@/components/Navigation.vue")
+  },
+  data() {
+    return {
+      title: "RPG do Mestre",
+      subtitle: "Onde o mestre cria, reinventa, modifica e publica RPG."
+    };
   }
 };
 </script>
@@ -27,6 +35,7 @@ body {
     "Bitstream Charter", "Nimbus Roman No9 L", Garamond, "Apple Garamond",
     "ITC Garamond Narrow", "New Century Schoolbook", "Century Schoolbook",
     "Century Schoolbook L", Georgia, serif;
+  overflow-x: hidden;
 }
 
 a {
@@ -113,26 +122,49 @@ h6 {
   }
 }
 
-p {
-  font-size: 1em;
-  line-height: 1.5em;
+p,
+a {
+  font-size: 1rem;
+  line-height: 1.5rem;
 
   @media screen and (min-width: 40em) {
-    font-size: 1.25em;
+    font-size: 2rem;
   }
 
   @media screen and (min-width: 60em) {
-    font-size: 1.5em;
+    font-size: 2.5rem;
   }
 }
 
 #app {
-  margin: 5%;
+  margin: 0 5% 3rem;
   width: 90%;
 
   @media screen and (min-width: 50em) {
-    margin: 2.5em auto;
+    margin: 0 auto 3rem;
     max-width: 50em;
+  }
+}
+
+section {
+  padding: 6em 0 3em;
+  position: relative;
+
+  &:before,
+  &:after {
+    content: " ";
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+  }
+
+  &:before {
+    right: 100%;
+  }
+
+  &:after {
+    left: 100%;
   }
 }
 </style>
