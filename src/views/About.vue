4deb516897e430930257d3473a0236d7cb3b1482
erick patrick <github@erickpatrick.net>
2018-07-24 15:01:51 +0200

    updates copy for 'About' view component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="about">
+    <h4>Nossa história</h4>
+    <p>O RPG do Mestre é um site sobre RPG criado em meados de 2009 por <a
+      href="https://twitter.com/erickpatrick">Erick Patrick</a>, ainda no formato de blog, que trazia artigos
+      semanalmente, com uma proposta diferente dos blogs da época: conteúdo diverso sem tratar sobre qualquer sistema
+      em específico.</p>
+    <p>Com o tempo, passou a publicar diariamente, com a ajuda de grandes amigos e colaboradores, especialmente do <a
+      href="https://twitter.com/delAngeles">Fernando del Angeles</a> (hoje na <a href="http://retropunk.net">Editora
+      Retropunk</a>) e despontou como um dos grandes blogs do Brasil na época.</p>
+    <p>O site, depois de um tempo, também passou a contar com um podcast, o Papo de Mestre. Assim como a maioria dos
+      podcasts da época, possuia frequência semanal e abordava temas diversos, assim como os artigos do blog. Foram
+      mais de 40 edições.</p>
+    <h4>A queda</h4>
+    <p>Infelizmente, em 2012, após um problema no servidor em que o site ficava, todos os mais de 700 artigos foram
+      perdidos e não havia <em>backup</em> desse trabalho todo, exceto por alguns podcasts foram salvos (cerca de 30
+      edições).</p>
+    <p>Isso desmotivou bastante a equipe, principalmente Erick, fazendo com que o RPGdM passasse por um
+      longo tempo sem atualizações.</p>
+    <h4>Presente e futuro</h4>
+    <p>Houve várias tentativas de retornar. Pensamos vários modos de voltar a publicar: Wordpress.org, Medium,
+      WordPress num servidor próprio (de novo), inclusive um layout de 2014 estava nessa página até pouco
+      tempo.</p>
+    <p>Porém, dessa vez, sem a pressão de publicar todos os dias, o site voltou. E com uma nova cara e objetivo: publicar
+      sistemas, cenários, aventuras, personagens, regras alternativas...</p>
+    <p>Dessa forma, quando eles estiverem prontos ou bons suficientes para serem apresentados (imagine uma versão
+      <em>alpha</em>/<em>beta</em>) eles serão publicados no site, onde os visitantes poderão acompanhar seu desenvolvimento.</p>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  background-color: #f9f9f9;
+
+  &:before,
+  &:after {
+    background-color: #f9f9f9;
+  }
+
+  h4:nth-child(n + 2) {
+    margin-top: 2em;
+  }
+}
+</style>
