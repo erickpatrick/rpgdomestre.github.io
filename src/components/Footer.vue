4328c10f0b7a6af7853b1df94985e70b23608faa
Erick Patrick <erick.alvarenga@nintendo.de>
2018-07-24 14:52:15 +0200

    adds 'Navigation' and 'Footer' components and makes (sub)title dynamic

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <p>&copy; 2009-{{ today }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  computed: {
+    today() {
+      return new Date().getFullYear();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  text-align: center;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <router-link tag="li" to="/">
+      <a>Home</a>
+    </router-link>
+    <router-link tag="li" to="/sobre">
+      <a>Sobre</a>
+    </router-link>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  margin-bottom: 4em;
+  padding-left: 0;
+  list-style: none;
+  text-align: center;
+
+  li {
+    display: inline-block;
+    padding: 1em;
+  }
+}
+</style>
