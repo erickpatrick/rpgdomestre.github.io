6346a5bbb134ce0d4c9a96bab227bdba887c2266
erick patrick <github@erickpatrick.net>
2018-07-27 01:38:03 +0200

    creates simple `table of contents` components
    misses:
    - property to properly set float side
    - some other things

---
+++ src/components/TableOfComponents.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="table-of-contents">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.table-of-contents {
+  margin-bottom: 1.5em;
+  padding: 1.5em;
+  background-color: #f9f9f9;
+
+  @media screen and (min-width: 50em) {
+    margin-left: 1.5em;
+    float: right;
+    max-width: 350px;
+  }
+
+  h4,
+  h5,
+  h6 {
+    margin-bottom: 0;
+  }
+
+  h4 {
+    font-size: 1.5em;
+  }
+
+  h5 {
+    font-size: 1.25em;
+    margin: 1em 0 0.5em 0;
+  }
+
+  h6,
+  p {
+    font-size: 1em;
+    line-height: 1.15;
+  }
+
+  p {
+    margin: 0 0 0.5em 1em;
+  }
+}
+</style>
