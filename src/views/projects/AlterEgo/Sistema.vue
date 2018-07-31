21b2f44587b81fe4e99087e09685fd445e782447
Erick Patrick <contato@erickpatrick.net>
2018-07-31 23:45:43 +0200

    adds extra content and styling

--- src/views/projects/AlterEgo/Sistema.vue
+++ src/views/projects/AlterEgo/Sistema.vue
@@ -10,7 +10,7 @@
                 <h5><a href="#regras">As Regras</a></h5>
                 <p><a href="#dados">Dados</a></p>
                 <p><a href="#atributos">Atributos</a></p>
-                <p>Resoluções Situacionais</p>
+                <p><a href="#resolucoes-situacionais">Resoluções Situacionais</a></p>
                 <p>Combate</p>
                 <p>Aptidões e Inépcias</p>
                 <p>Dons</p>
@@ -55,48 +55,117 @@
                     <section id="atributos">
                         <h6>Atributos</h6>
                         <p>Os Atributos no Sistema <em><strong>Alter Ego</strong></em> tem por objetivo refletir as várias capacidades físicas e mentais que um personagem possui. Os personagens tiveram seus atributos divididos em 2 categorias: Físicos e Mentais. Somado a isso, temos o atributo especial, Saúde, que indica a saúde física e mental do personagem.</p>
-                      <p>Os atributos Físicos e Mentais começam em zero, mas podem variar de <strong>-5 a 10</strong>. Cada valor de
+                        <p>Os atributos Físicos e Mentais começam em zero, mas podem variar de <strong>-5 a 10</strong>. Cada valor de
                         atributo fará parte da quantidade d6 (<em>pilha de dados</em>) que o jogador lançará nas
                         resoluções situacionais (<em>rolagens de dados</em>).</p>
-                      <p>O jogador, no ato da criação do personagem, terá a seu dispor 10 pontos para gastar entre os atributos Físicos e Mentais. Pode-se retirar pontos de um atributo para fazer com que outros tenham valores mais altos. A soma dos atributos negativos não pode ser menor que -5. O valor da Saúde é calculado de modo especial: Ele é a soma dos atributos Físicos. </p>
+                        <p>O jogador, no ato da criação do personagem, terá a seu dispor 10 pontos para gastar entre os atributos Físicos e Mentais. Pode-se retirar pontos de um atributo para fazer com que outros tenham valores mais altos. A soma dos atributos negativos não pode ser menor que -5. O valor da Saúde é calculado de modo especial: Ele é a soma dos atributos Físicos. </p>
+                        <p class="h7"><strong>Físicos</strong></p>
+                        <ul>
+                            <li>
+                                <strong>Força (FN)</strong>: Trata da capacidade física do personagem. Qualquer ação ou situação que requeira o uso específico de força ou músculos, esse atributo será usado.
+                            </li>
+                            <li>
+                                <strong>Destreza (DX)</strong>: Trata da capacidade de usar suas mãos e o quão bem você é capaz de usá-las nas diversas situações que aparecerem. Se você tem de usar suas mãos para controlar algo; atirar usando armas de fogo, arco e flecha, machadinhas e similares; fazer mímicas; digitação acelerada; tocar instrumentos; e similares, esse atributo será usado.
+                            </li>
+                            <li>
+                                <strong>Mobilidade (MB)</strong>: Trata da capacidade de movimentar-se ou usar seu corpo como um todo para movimentar-se pelo ambiente; mexer o próprio corpo em situações adversas; entre outros momentos. Para esses e outros tipos de ações este atributo será usado.
+                            </li>
+                        </ul>
+                        <p class="h7"><strong>Mentais</strong></p>
+                        <ul>
+                            <li>
+                                <strong>Conhecimento (CN)</strong>: Tudo relacionado à atividade de pensar, memorizar, lembrar, raciocinar e relacionados estão sob o escopo desse atributo.
+                            </li>
+                            <li>
+                                <strong>Comportamento (CM)</strong>: O modo de agir no dia-a-dia, as interações sociais e interpessoais estão sob a tutela desse atributo. Intimidar e/ou enganar pessoas para o ter informações também caem nesse grupo.
+                            </li>
+                            <li>
+                                <strong>Vontade (VN)</strong>: A capacidade de aguentar e lidar com pressão psicológica, estresse mental, reflexão mental, entre outros caem sob a batuta desse atributo.
+                            </li>
+                        </ul>
+                        <p class="h7"><strong>Saúde (SD) e Pontos de Vida (PV)</strong></p>
+                        <p>Soma-se os valores de <strong>FR</strong>, <strong>DX</strong> e <strong>MB</strong>. É a mesma quantidade de <strong><em>Pontos de Vida (PV)</em></strong>. <em>-Saúde</em> é o valor <em>mínimo</em> que <strong>PV</strong> que um personagem pode chegar antes de morrer. <strong>+Saúde</strong> é o valor máximo de pontos de vida naturais que um personagem possui.</p>
+                        <blockquote>
+                            <p><strong>Observação</strong>: Se, por algum motivo, um atributo chegar a ficar em <strong>-6 ou menor</strong>, ele fica <em>desabilitado</em>, ou seja, não pode ser usado na rolagem básica, porém a rolagem ainda pode ser feita, caracterizando a improvisação ou força de vontade da personagem, caso ainda seja possível realizar uma <strong>Jogada Desesperada</strong>.</p>
+                        </blockquote>
                     </section>
                     <section>
-                        <h6>Resoluções Situacionais</h6>
+                        <h6 id="resolucoes-situacionais">Resoluções Situacionais</h6>
+                        <p>As Resoluções Situacionais são as conhecidas rolagens de dados. Elas que resolverão os momentos de incerteza e risco que acontecerão em suas sessões de jogo.</p>
+                        <p class="h7"><strong>Dificuldades</strong></p>
+                        <ul>
+                            <li><strong>Moleza:</strong> +3d. Sucesso em 3, 4, 5 e 6 (67% de chance de acerto em cada dado)</li>
+                            <li><strong>Fácil:</strong> +1d. Sucesso em 4, 5 e 6 (50%)</li>
+                            <li><strong>Médio:</strong> +0d. Sucesso em 4, 5 e 6 (50%)</li>
+                            <li><strong>Difícil:</strong> -1d. Sucesso em 5 e 6 (34%)</li>
+                            <li><strong>Extrema:</strong> -3d. Sucesso em 5 e 6 (34%)</li>
+                            <li><strong>Hercúlea:</strong> -5d. Sucesso em 6 (17%)</li>
+                            <li><strong>Divina:</strong> -7d. Sucesso em 6 (17%)</li>
+                            <li><strong>Impossível?!:</strong> -9d e -1 sucessos. Sucesso em 6 (17%).</li>
+                        </ul>
+                        <p class="h7"><strong>Pilha de Dados</strong></p>
+                        <p>É a quantidade de dados que um jogador tem para resolver uma determinada situação. Ela é calculada da seguinte forma:</p>
+                        <ul><li><strong>Pilha de Dados</strong> = Valor Atributo Base + Dificuldade + Dons</li></ul>
+                        <blockquote>
+                            <p>John quer que seu personagem tente saltar de um prédio para outro. O mestre diz que a dificuldade de tal salto, dada a distância entre os prédios, é <em>Divina</em>. Como a Mobilidade (<em>Atributo Base</em> para esse teste) do personagem dele é 1, a <em>Pilha de Dados</em> de John é -6 (-7 + 1). Com essa pilha, o personagem de John não será capaz de realizar a tarefa porque, digamos, seus instintos disseram que não seria possível.</p>
+                        </blockquote>
+                        <p class="h7"><strong>Resolução</strong></p>
+                        <p>Para saber se um personagem é bem sucedido em sua ação, o jogador lança sua Pilha de Dados e verifica quantos dados resultaram em Sucesso, dada a dificuldade da ação, porém, para cada resultado 1 no dado, anula-se um Sucesso. Se ainda houver sucessos após a anulação, o personagem é bem sucedido.</p>
+                        <blockquote>
+                            <p>John possui uma <em>Pilha de Dados</em> com 3 dados para uma situação <em>Difícil</em>. Após rolá-los, ele obtém os seguintes valores: 1, 5 e 6. Ele obteve 2 Sucessos e 1 Falha. Como a Falha foi um resultado 1, anula-se um dos Sucessos, deixando-o com 1 Sucesso, o que garante que o personagem suceda na situação.</p>
+                            <p>Em uma outra situação, a <em>Pilha de Dados</em> de John é 5 para um situação <em>Difícil</em>. Após rolar os dados de sua pilha, ele obteve os seguintes resultados: 2, 4, 4, 3, e 5. Como não houve dados com número 1, nada é cancelado e ele obtém um sucesso ao final.</p>
+                        </blockquote>
+                        <p class="h7"><strong>Sucesso Decisivo</strong></p>
+                        <p>Será <strong>Sucesso Decisivo</strong> se metade (arredondado para cima) ou mais dos dados lançados resultarem em 6 e todos os dados forem Sucesso (não válido para <em>Jogada Desesperada</em>)</p>
+                        <p>Os danos e efeitos aumentam com os multiplicadores concedidos pelas armas, itens, dons, etc.</p>
+                        <blockquote>
+                            <p>John tem uma <em>Parada de Dados</em> com 5 dados no total, para uma situação Difícil. Na rolagem ele obteve: 6, 5, 6, 5, 6. Todos seus dados foram Sucesso. Agora ele conta quantos 6 ele obteve: 3! Como 3 é a metade de 5 (arredondado para cima), ele conseguiu um <em><strong>Sucesso Decisivo</strong></em>.</p>
+                        </blockquote>
+                        <p class="h7"><strong>Falha Crítica</strong></p>
+                        <p>Será <strong>Falha Crítica</strong> se metade ou mais dos dados (arredondado para cima) resultarem em 1 e não houver qualquer sucesso.</p>
+                        <p>Assim como no <em>Sucesso Decisivo</em>, os danos e efeitos diminuem com os multiplicadores concedidos pelas armas, itens, dons, etc.</p>
+                        <blockquote>
+                            <p>John tem outra <em>Parada de Dados</em> com 5 dados no total para uma situação Difícil. Na rolagem, dessa vez, ele obteve: 1, 4, 3, 1, 1. Todos seus dados foram Falhas. Agora ele conta quantos 1 ele obteve: 3! Como 3 é a metade de 5 (arredondado para cima), infelizmente, ele obteve um <em><strong>Falha Crítica</strong></em>.</p>
+                        </blockquote>
+                        <p class="h7"><strong>Jogada Desesperada</strong></p>
+                        <p>Se a Pilha de dados for <strong>nula</strong> (ou seja, 0 dados) ou ficar negativa até o mínimo de <strong>-5</strong>, o jogador poderá lançar um dado, valendo como <strong>Jogada Desesperada</strong>. O jogador somente será bem sucedido se obtiver <em>6</em> e esse não contará como <strong>Sucesso Decisivo</strong>. Se obtiver <em>1</em>, terá de lançar outro dado. Se não obtiver <em>6</em>, será considerado uma <strong>Falha Crítica</strong>.</p>
+                        <blockquote>
+                            <p>Após alguns meses de aventuras, o personagem de John precisa tentar, novamente, saltar de um prédio para outro. O mestre diz que a dificuldade de tal salto, dada a distância entre os prédios, é <em>Hercúlea</em>. Como a Mobilidade (Atributo Base para esse teste) do personagem dele agora é 4, a Pilha de Dados de John é -1 (-5 + 4).. Como o valor -1 é maior ou igual a -5, o personagem de John poderá tentar um salto desesperado, através da <em>Jogada Desesperada</em>, tendo uma chance de alcançar o outro prédio.</p>
+                        </blockquote>
+                    </section>
+                    <section>
+                        <h6 id="combate">Combate</h6>
                         <p>Conteúdo em breve</p>
                     </section>
                     <section>
-                        <h6>Combate</h6>
+                        <h6 id="aptidoes-inepcias">Aptidões e Inépcias</h6>
                         <p>Conteúdo em breve</p>
                     </section>
                     <section>
-                        <h6>Aptidões e Inépcias</h6>
-                        <p>Conteúdo em breve</p>
-                    </section>
-                    <section>
-                        <h6>Dons</h6>
+                        <h6 id="dons">Dons</h6>
                         <p>Conteúdo em breve</p>
                     </section>
                 </article>
                 <article>
-                    <h4>Listas</h4>
+                    <h4 id="listas">Listas</h4>
                     <section>
-                        <h6>Situações x Testes</h6>
+                        <h6 id="situacoes-x-testes">Situações x Testes</h6>
                         <p>Conteúdo em breve</p>
                     </section>
-                    <section>
+                    <section id="armas-e-protecoes">
                         <h6>Armas e proteções</h6>
                         <p>Conteúdo em breve</p>
                     </section>
-                    <section>
+                    <section id="aptidoes">
                         <h6>Aptidões</h6>
                         <p>Conteúdo em breve</p>
                     </section>
-                    <section>
+                    <section id="inepcias">
                         <h6>Inépcias</h6>
                         <p>Conteúdo em breve</p>
                     </section>
-                    <section>
-                        <h6>Outros</h6>
+                    <section id="extras">
+                        <h6>Extras</h6>
                         <p>Conteúdo em breve</p>
                     </section>
                 </article>
@@ -131,6 +200,11 @@ export default {
     article {
       margin-bottom: 6em;
     }
+  }
+
+  .h7 {
+      margin-top: 1.5em;
+      color: #666;
   }
 
   a {
