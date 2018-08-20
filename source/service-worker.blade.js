1c86f48d99cdf60b3465447a6803e325ac2a75eb
Erick Patrick <erick.alvarenga@nintendo.de>
2018-08-20 17:24:48 +0200

    hotfix to add link to weekly #1

--- source/service-worker.blade.js
+++ source/service-worker.blade.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rpgdomestre-cached-version-5';
+const CACHE_NAME = 'rpgdomestre-cached-version-5.0.1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -27,7 +27,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['rpgdomestre-cached-version-5'];
+  var cacheWhitelist = ['rpgdomestre-cached-version-5.0.1'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
