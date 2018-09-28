f117bc8a2bb0d500d9affd1f1a2620ab4fec3b6f
Erick Patrick <erick.alvarenga@nintendo.de>
2018-09-28 14:36:00 +0200

    removes old folder to see if new folder picks up

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <style>
+            {{ inline(mix('css/main.css', 'assets/build')) }}
+        </style>
         <title>@yield('title', $page->site->title)</title>
     </head>
     <body>
