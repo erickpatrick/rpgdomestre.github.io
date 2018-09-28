48a820513bde9bb5d0ca273c8cb55827f33c64b2
erick patrick <github@erickpatrick.net>
2018-09-28 14:42:49 +0200

    fixes inline non existing on default jigsaw

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <style>
-            {{ inline(mix('css/main.css', 'assets/build')) }}
+            {{ mix('css/main.css', 'assets/build') }}
         </style>
         <title>@yield('title', $page->site->title)</title>
     </head>
