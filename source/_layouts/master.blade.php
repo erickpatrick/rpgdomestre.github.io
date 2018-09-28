4a03f2c8370e634ad69c6a7c6c07a940acd8e876
Erick Patrick <erick.alvarenga@nintendo.de>
2018-09-28 14:44:39 +0200

    fixes printing styles directly

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <style>
-            {{ mix('css/main.css', 'assets/build') }}
-        </style>
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <title>@yield('title', $page->site->title)</title>
     </head>
     <body>
