4d77f039cdcaed69c5026c8270db21524e1608ad
Erick Patrick <erick.alvarenga@nintendo.de>
2019-03-15 12:07:40 +0100

    associates manifest with the page =)

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel='manifest' href='{{ $page->baseUrl }}/manifest.json'>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <title>@yield('title', $page->site->title)</title>
     </head>
