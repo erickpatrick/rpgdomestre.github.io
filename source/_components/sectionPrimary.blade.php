4b9309ff25a91658bf394022aac178c2fbfb45b6
erick patrick <github@erickpatrick.net>
2019-08-19 00:22:45 +0200

    allows passing classes, just as sectionSecondary

--- source/_components/sectionPrimary.blade.php
+++ source/_components/sectionPrimary.blade.php
@@ -1,4 +1,4 @@
-<section class="section section--primary">
+<section class="section section--primary {{ $classes ?? ''}}">
     @if(isset($title))
     <h4 class="section__title">{{ $title }}</h4>
     @endif
