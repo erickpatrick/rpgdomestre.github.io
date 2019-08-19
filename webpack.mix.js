f792cbfe946bd76f176ac52a215663e7492c6b6b
erick patrick <github@erickpatrick.net>
2019-08-19 23:10:28 +0200

    updates jigsaw system

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,34 @@
-let mix = require('laravel-mix')
-let build = require('./tasks/build.js')
+let mix = require('laravel-mix');
+let build = require('./tasks/build.js');
+require('laravel-mix-purgecss');
 
-mix.disableSuccessNotifications()
-mix.setPublicPath('source/assets/build')
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build/');
 mix.webpackConfig({
-  plugins: [
-    build.jigsaw,
-    build.browserSync(),
-    build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-  ]
-})
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch([
+            'config.php',
+            'source/**/*.md',
+            'source/**/*.php',
+            'source/**/*.scss',
+            '!source/**/_tmp/*'
+        ]),
+    ],
+});
 
 mix.js('source/_assets/js/main.js', 'js')
-  .sass('source/_assets/sass/main.scss', 'css')
-  .options({
-    processCssUrls: false,
-  }).version()
+    .sourceMaps()
+    .sass('source/_assets/sass/main.scss', 'css/main.css')
+    .sourceMaps()
+    .options({
+        processCssUrls: false,
+        postCss: [],
+    })
+    .purgeCss({
+        extensions: ['html', 'md', 'js', 'php', 'vue'],
+        folders: ['source'],
+        whitelistPatterns: [/language/, /hljs/],
+    })
+    .version();
