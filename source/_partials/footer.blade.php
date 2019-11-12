1e2303898066cb7b5d77e93c7bbdd0a4dbc5e8e5
erick patrick <github@erickpatrick.net>
2019-11-12 16:38:29 +0100

    updates footer structure to newsest layout

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,3 +1,5 @@
-<footer class="footer">
-    @copyright(['year' => date('Y')])
-</footer>
+<div class="mt-20 px-5 bg-blue-700">
+    <div class="py-5 container mx-auto">
+        <p class="text-center text-white">&copy; 2009-{{ date('Y') }}</p>
+    </div>
+</div>
