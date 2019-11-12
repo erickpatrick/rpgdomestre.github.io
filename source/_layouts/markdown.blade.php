ca796d6c71019e5109a60f4f1f956aeab6591845
erick patrick <github@erickpatrick.net>
2019-11-12 16:40:24 +0100

    updates markdown layout to use new design structure

--- source/_layouts/markdown.blade.php
+++ source/_layouts/markdown.blade.php
@@ -3,34 +3,7 @@
 @section('description', substr($page->description,0,149).'...')
 
 @section('header')
-    @header(['page' => $page, 'subheader' => $page->mSubheader])
-        @slot('title', $page->mUseMTitleForPageTitle ? $page->mTitle : $page->site->title)
-        @slot('subtitle', $page->mUseMTitleForPageTitle ? $page->mSubtitle : $page->site->subtitle)
-    @endheader
-@endsection
-
-@section('navigation')
-    @if($page->mNavigation)
-        @navigation([
-            'items' => [
-                [
-                    'title' => 'Home', 
-                    'url' => $page->urlTo(), 
-                    'active' => false
-                ],
-                [
-                'title' => 'Weekly',
-                'url' => $page->urlTo('weekly'),
-                'active' => $page->mActive
-                ],
-                [
-                    'title' => 'Sobre', 
-                    'url' => $page->urlTo('sobre'), 
-                    'active' => $page->mActive
-                ]
-            ]
-        ])
-    @endif
+    @include('_partials.header.internal')
 @endsection
 
 @section('title')
@@ -38,21 +11,9 @@
 @endsection
 
 @section('content')
-<div class="home {{ $page->mContentClasses }}">
-    @if($page->mSectionPrimary)
-        @sectionPrimary
-    @endif
-
-    @if($page->mToc)
-        <div class="table-of-contents">
-        <h5>Índice</h5>
-        @include($page->mToc)
-        </div>
-    @endif
-    @yield('mContent')
-
-    @if($page->mSectionPrimary)
-        @endsectionPrimary
-    @endif
+<div class="mt-20 px-5 min-h-screen">
+    <div class="py-5 container mx-auto">
+        @yield('mContent')
+    </div>
 </div>
 @endsection
