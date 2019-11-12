d10e2be255305fda70275ea80a7863be75d9c2ea
erick patrick <github@erickpatrick.net>
2019-11-12 16:21:30 +0100

    removes unneeded partials

--- source/_partials/navigation.blade.php
+++
@@ -1,6 +0,0 @@
-<ul class="navigation">
-@foreach ($items as $item)
-    <li class="navigation__link{{ !$item['active'] ? '' : ' navigation__link--active' }}">
-    <a href="{{ $item['url'] }}">{{ $item['title'] }}</a></li>
-@endforeach
-</ul>
--- source/_partials/subheader.blade.php
+++
@@ -1,3 +0,0 @@
-<section class="section section--sub-heading">
-    <a href="{{ $page->baseUrl }}" class="router-link-active"><strong>RPG do Mestre</strong></a>&nbsp;apresenta
-</section>
