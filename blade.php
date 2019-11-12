88376570a9c6eeada0667ca1b99bb504a2c17135
erick patrick <github@erickpatrick.net>
2019-11-12 16:20:01 +0100

    removes unnecessary components

--- blade.php
+++ blade.php
@@ -1,17 +1,3 @@
 <?php
 
 /** @var \Illuminate\View\Compilers\BladeCompiler $bladeCompiler */
-
-/** components */
-$bladeCompiler->component('_components.header');
-$bladeCompiler->component('_components.subheader');
-$bladeCompiler->component('_components.sectionPrimary');
-$bladeCompiler->component('_components.sectionSecondary');
-$bladeCompiler->component('_components.column');
-$bladeCompiler->component('_components.toc');
-
-/** named includes: easy helpers */
-$bladeCompiler->include('_partials.navigation');
-$bladeCompiler->include('_partials.copyright');
-$bladeCompiler->include('_partials.footer');
-$bladeCompiler->include('_partials.projectListing');
