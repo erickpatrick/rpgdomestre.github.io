937ab13afd2a1cf97efbe3cb38a55374c3e337b1
erick patrick <github@erickpatrick.net>
2019-11-12 16:20:57 +0100

    removes unnecessary components files

--- source/_components/column.blade.php
+++
@@ -1,7 +0,0 @@
-<section>
-    <h6>{{ $title }}</h6>
-    {{ $content }}
-    @if($showLink)
-    <a href="{{ $link ?? '#' }}" class="">Visitar</a>
-    @endif
-</section>
--- source/_components/sectionSecondary.blade.php
+++
@@ -1,3 +0,0 @@
-<section class="section section--secondary {{ $classes }}">
-    {{ $slot }}
-</section>
