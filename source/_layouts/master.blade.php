e9e187281a30561d3ffd8c323f7b51ecaa8e02c9
erick patrick <github@erickpatrick.net>
2019-11-12 17:48:52 +0100

    makes page use compiled tailwindcss

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,16 +7,9 @@
         <meta name="description" content="@yield('description')">
         <meta name="keywords" content="{{ $page->site->keywords  }}">
         <link rel='manifest' href='{{ $page->baseUrl }}/manifest.json'>
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-        {{-- <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}"> --}}
+        {{-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> --}}
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <title>@yield('title', $page->site->title)</title>
-        <style>
-            @media (min-width: 768px) {
-                .md\:text-8xl {
-                    font-size: 6rem;
-                }
-            }
-        </style>
     </head>
     <body class="font-serif">
         @yield('header')
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '3.5rem',
+      '7xl': '4rem',
+      '8xl': '6rem',
+    }
+  },
+  variants: {
+    width: ['responsive', 'focus']
+  },
+  plugins: [
+  ]
+}
