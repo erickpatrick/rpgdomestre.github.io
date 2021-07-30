38461a0fc637d4926b2849cb2ef46563b819ca25
erick patrick <github@erickpatrick.net>
2021-07-31 00:25:09 +0200

    adds layout for most internal pages

---
+++ components/layouts/InternalLayout.js
@@ -0,0 +1,27 @@
+import Main from "./Main";
+import HeaderInternal from "../elements/Header/Internal";
+import Meta from "../elements/Meta";
+
+export default function InternalLayout({ children, meta }) {
+  return (
+    <>
+      <Main>
+        <Meta title={meta?.title ?? ""} description={meta?.description} />
+        <div className="container mx-auto p-4">
+          <HeaderInternal />
+          <div
+            className={`prose prose-${meta?.color} prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto py-20`}
+          >
+            <h6
+              className={`sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-${meta?.color}-600 mb-3`}
+            >
+              Weekly
+            </h6>
+            <h1>{meta?.title}</h1>
+            {children}
+          </div>
+        </div>
+      </Main>
+    </>
+  );
+}
