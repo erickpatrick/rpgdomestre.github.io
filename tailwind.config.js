946f2dc59a2bb6a2af887e6738ec25c41d3e73ee
erick patrick <github@erickpatrick.net>
2021-07-31 00:15:31 +0200

    adds nexjs

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      "text-yellow-600",
+      "hover:text-yellow-800",
+      "text-blue-600",
+      "hover:text-blue-800",
+      "text-pink-600",
+      "hover:text-pink-800",
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
