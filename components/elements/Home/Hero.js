64a87f851995302b8a0ee970898459611ad8975c
erick patrick <github@erickpatrick.net>
2021-07-31 00:52:17 +0200

    creates HomeHero component

---
+++ components/elements/Home/Hero.js
@@ -0,0 +1,19 @@
+export default function HomeHero() {
+  return (
+    <div className="flex items-center justify-center py-16 md:py-32">
+      <img
+        className="w-20 sm:w-44"
+        src="images/mestre.jpg"
+        alt="Logo RPG do Mestre"
+      />
+      <h1 className="font-bold">
+        <span className="block -mb-2 sm:-mb-5 text-3xl sm:text-7xl font-semibold text-transparent bg-gradient-to-r bg-clip-text from-blue-600 to-pink-600">
+          RPG do
+        </span>
+        <span className="block text-5xl sm:text-9xl font-black text-transparent bg-gradient-to-r bg-clip-text from-pink-600 to-yellow-300">
+          Mestre
+        </span>
+      </h1>
+    </div>
+  );
+}
