fbbb0a5bfca87ca1387478acda66b7443469af06
erick patrick <github@erickpatrick.net>
2021-08-10 15:36:08 +0200

    updates rate limit number of requests to 60

--- pages/api/contact/create.js
+++ pages/api/contact/create.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 
   try {
     // 3 requests per minute
-    await limiter.check(res, 5, "CONTACT_CREATE_CACHE_TOKEN");
+    await limiter.check(res, 10, "CONTACT_CREATE_CACHE_TOKEN");
     await handlers[req.method]();
   } catch {
     return res.status(429).json({ error: "Rate limit exceeded" });
