0608438203b9576ffd12fedff2d8ce543e735768
erick patrick <github@erickpatrick.net>
2022-07-26 18:40:31 +0200

    removes preact to check compatibility on production

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const withMDX = require("@next/mdx")({
 });
 
 module.exports = withMDX({
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-
-    return config;
-  },
   reactStrictMode: true,
   experimental: {
     newNextLinkBehavior: true,
