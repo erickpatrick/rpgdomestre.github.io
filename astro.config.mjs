9a1605fd78e0b86c7dd88a3cb6658d7ebd221c4d
erick patrick <github@erickpatrick.net>
2022-08-27 19:55:05 +0200

    switches back to ssg instead of server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +10,4 @@ export default defineConfig({
     ? "http://localhost:3000"
     : "https://www.rpgdomestre.com",
   integrations: [tailwind(), sitemap(), image(), preact({ compat: true })],
-  output: "server",
-  adapter: vercel(),
 });
